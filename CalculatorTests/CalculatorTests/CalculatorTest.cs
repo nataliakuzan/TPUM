6686b772c24fe561b1b3e2841db4605e3a5aabc4
217851 Kuzan Natalia <217851@edu.p.lodz.pl>
2022-03-07 19:26:29 +0100

    Add Calculator Project

---
+++ CalculatorTests/CalculatorTests/CalculatorTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Calculator;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void AddMethodTest()
+        {
+            Calculator.Calculator calc = new Calculator.Calculator();
+            calc.Add(1, 2);
+            Assert.AreEqual(calc.Add(1, 2), 3);
+        }
+    }
+}
