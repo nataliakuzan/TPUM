006c343544a349c795786ee20caa7ef4e961d792
217851 Kuzan Natalia <217851@edu.p.lodz.pl>
2022-03-14 18:56:28 +0100

    Add Shop Project

---
+++ Shop/Shop/Shop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shop
+{
+    public class Shop
+    {
+        public int AddItem(int v1)
+        {
+            return v1;
+        }
+    }
+}
---
+++ ShopTests/ShopTests/ShopTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Shop;
+
+namespace ShopTests
+{
+    [TestClass]
+    public class ShopTests
+    {
+        [TestMethod]
+        public void AddItemTest()
+        {
+            Shop.Shop s = new Shop.Shop();
+            s.AddItem(1);
+        }
+    }
+}
