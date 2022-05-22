0c6eb998baf0af69ca7e3b85171746123454e66e
nataliakuzan <33835410+nataliakuzan@users.noreply.github.com>
2022-05-22 13:14:26 +0200

    Added WebSocket project

---
+++ Shop/ServerPresentation/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerPresentation
+{
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Server started");
+            await WebSocketServer.Server(8081, ConnectionHandler);
+        }
+
+        static void ConnectionHandler(WebSocketConnection webSocketConnection)
+        {
+            Console.WriteLine("[Server]: Client connected");
+            WebSocketServer.CurrentConnection = webSocketConnection;
+            webSocketConnection.onMessage = ParseMessage;
+            webSocketConnection.onClose = () => { Console.WriteLine("[Server]: Connection closed"); };
+            webSocketConnection.onError = () => { Console.WriteLine("[Server]: Connection error encountered"); };
+        }
+
+        static async void ParseMessage(string message)
+        {
+            Console.WriteLine($"[Client]: {message}");
+            if (message == "echo")
+            {
+                await SendMessageAsync("echoResponse");
+            }
+
+            if (message == "RequestAll")
+            {
+
+            }
+
+            if (message.Contains("RequestTransaction"))
+            {
+
+            }
+        }
+
+        static async Task SendMessageAsync(string message)
+        {
+            Console.WriteLine($"[Server]: {message}");
+            await WebSocketServer.CurrentConnection.SendAsync(message);
+        }
+    }
+    
+}
