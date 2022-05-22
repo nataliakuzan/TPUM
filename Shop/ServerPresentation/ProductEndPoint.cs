364a9896592f5c46f264402b744b9a3691984cf5
nataliakuzan <33835410+nataliakuzan@users.noreply.github.com>
2022-05-22 13:42:34 +0200

    Added EndPoint

---
+++ Shop/ServerPresentation/ProductEndPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerPresentation
+{
+    class ProductEndPoint
+    {
+        public string listAll()
+        {
+
+            return "product";
+        }
+
+    }
+}
---
+++ Shop/ServerPresentation/Route.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerPresentation
+{
+    class Route
+    {
+        private Dictionary<string, int> EndPointsRoute;
+    }
+}
